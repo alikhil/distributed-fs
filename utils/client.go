03dcf2d33e2afa3c1d7095a61657066dd5474c3f
Alik Khilazhev <alikhil@mail.ru>
2018-05-14 19:41:41 +0300

    Update client.go

--- utils/client.go
+++ utils/client.go
@@ -4,6 +4,9 @@ import (
 	"net/rpc"
 )
 
+// DFSClient is used in TBMS project, so it's left for backward compatability.
+// DEPRICATED
+// Recommended to use other RemoteDFS
 type DFSClient struct {
 	Client *rpc.Client
 }
@@ -39,4 +42,40 @@ func (dfs *DFSClient) WriteBytes(fname string, offset int32, data *[]byte) bool 
 func (dfs *DFSClient) CreateFile(fname string) bool {
 	ok := false
 	return dfs.Client.Call("RemoteIO.CreateFile", &fname, &ok) == nil && ok
+}
+
+type RemoteDFS struct {
+	Client *rpc.Client
+}
+
+func (dfs *RemoteDFS) InitRecordMappings(mp *map[string]int32) error {
+	ok := false
+	return dfs.Client.Call("RemoteIO.InitRecordMappings", mp, &ok)
+}
+
+func (dfs *RemoteDFS) FileExists(fname string) error {
+	ok := false
+	return dfs.Client.Call("RemoteIO.FileExists", &fname, &ok)
+}
+
+func (dfs *RemoteDFS) DeleteFile(fname string) error {
+	ok := false
+	return dfs.Client.Call("RemoteIO.DeleteFile", &fname, &ok)
+}
+
+func (dfs *RemoteDFS) ReadBytes(fname string, offset, count int32) ([]byte, error) {
+	data := make([]byte, count, count)
+
+	err := dfs.Client.Call("RemoteIO.ReadBytes", &IOReadArgs{Offset: offset, Count: count, Filename: &fname}, &data)
+	return data, err
+}
+
+func (dfs *RemoteDFS) WriteBytes(fname string, offset int32, data *[]byte) error {
+	ok := false
+	return dfs.Client.Call("RemoteIO.WriteBytes", &IOWriteArgs{Offset: offset, Data: data, Filename: &fname}, &ok)
+}
+
+func (dfs *RemoteDFS) CreateFile(fname string) error {
+	ok := false
+	return dfs.Client.Call("RemoteIO.CreateFile", &fname, &ok)
 }
